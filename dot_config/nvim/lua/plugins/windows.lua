e9f03f28e30d98a1dd94fc6ca4bfb4dfe7bbc2a3
Mikko Haapanen <26046991+haapmik@users.noreply.github.com>
2023-08-30 00:37:18 +0300

    Tweaked window animation

--- dot_config/nvim/lua/plugins/windows.lua
+++ dot_config/nvim/lua/plugins/windows.lua
@@ -13,6 +13,13 @@ function M.config()
       enable = true,
       duration = 50,
       fps = 60,
+      easing = function(r) -- https://easings.net/#easeOutExpo
+        if r == 1 then
+          return 1
+        else
+          return 1 - math.pow(2, -10 * r)
+        end
+      end,
     },
   })
 end
