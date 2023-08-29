4cfada3afe226a99ef2286b6d08251db35ba03e3
Mikko Haapanen <26046991+haapmik@users.noreply.github.com>
2023-08-30 01:26:16 +0300

    Added diagnostics information to folds

--- dot_config/nvim/lua/plugins/pretty-fold.lua
+++ dot_config/nvim/lua/plugins/pretty-fold.lua
@@ -10,9 +10,33 @@ function M.config()
     sections = {
       left = {
         "content",
-        "… ",
+        "…",
         function()
-          return vim.v.foldend - vim.v.foldstart + 1
+          return string.format(" %d lines ", vim.v.foldend - vim.v.foldstart + 1)
+        end,
+        function()
+          local value = ""
+          local error_count = 0
+          local warn_count = 0
+
+          for i = vim.v.foldstart, vim.v.foldend, 1 do
+            error_count = error_count + #vim.diagnostic.get(0, { lnum = i, severity = vim.diagnostic.severity.ERROR })
+            warn_count = warn_count + #vim.diagnostic.get(0, { lnum = i, severity = vim.diagnostic.severity.WARN })
+          end
+
+          if error_count > 0 then
+            value = value .. string.format(" %d E ", error_count)
+          end
+
+          if warn_count > 0 then
+            value = value .. string.format(" %d W ", warn_count)
+          end
+
+          if value ~= "" then
+            value = ":" .. value
+          end
+
+          return value
         end,
       },
     },
