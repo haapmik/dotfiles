74afc7fec8a13d3484467b399677e5552d44ec00
Mikko Haapanen <26046991+haapmik@users.noreply.github.com>
2023-09-26 17:11:15 +0300

    Updated tmux theme colors

--- dot_config/tmux/executable_theme.sh
+++ dot_config/tmux/executable_theme.sh
@@ -6,29 +6,31 @@
 main() {
     ####
     # COLOR PALETTE
-    local color_bg="#2e323b"
-    local color_fg="#282c34"
-    local color_green="#98c379"
-    local color_yellow="#e5c07b"
-    local color_red="#e06c75"
-    local color_blue="#61afef"
-    local color_cyan="#56b6c2"
-    local color_purple="#c678dd"
+    # Based on Snazzy: https://github.com/sindresorhus/hyper-snazzy
+    local color_fg="#eff0eb"
+    local color_bg="#282a36"
+    local color_green="#5af78e"
+    local color_yellow="#f3f99d"
+    local color_red="#ff5c57"
+    local color_blue="#57c7ff"
+    local color_cyan="#9aedfe"
+    local color_magenta="#ff6ac1"
     local color_gray="#5c6370"
     local color_buffer="#939aa3"
-    local color_selection="#3e4452"
+    local color_border="#222430"
+
 
     ####
     # PLUGINS
 
     tmux set -g @mode_indicator_prefix_prompt " PREFIX "
-    tmux set -g @mode_indicator_prefix_mode_style bg=$color_yellow,fg=$color_bg,bold
+    tmux set -g @mode_indicator_prefix_mode_style bg="$color_yellow,fg=$color_bg",bold
     tmux set -g @mode_indicator_copy_prompt "  COPY  "
-    tmux set -g @mode_indicator_copy_mode_style bg=$color_purple,fg=$color_bg,bold
+    tmux set -g @mode_indicator_copy_mode_style bg="$color_magenta,fg=$color_bg",bold
     tmux set -g @mode_indicator_sync_prompt "  SYNC  "
-    tmux set -g @mode_indicator_sync_mode_style bg=$color_red,fg=$color_bg,bold
+    tmux set -g @mode_indicator_sync_mode_style bg="$color_red,fg=$color_bg",bold
     tmux set -g @mode_indicator_empty_prompt "  TMUX  "
-    tmux set -g @mode_indicator_empty_mode_style bg=$color_blue,fg=$color_bg,bold
+    tmux set -g @mode_indicator_empty_mode_style bg="$color_blue,fg=$color_bg",bold
 
     # tmux cpu
     tmux set -g @cpu_percentage_format "CPU: %2.0f%%"
@@ -48,9 +50,9 @@ main() {
     tmux set -g @ram_high_thresh "80"
 
     # tmux-pomodoro
-    tmux set -g @pomodoro_on " #[fg=$color_gray] "
-    tmux set -g @pomodoro_complete " #[fg=$color_green] "
-    tmux set -g @pomodoro_ask_break " #[fg=$color_gray] break?"
+    #tmux set -g @pomodoro_on " #[fg=$color_gray] "
+    #tmux set -g @pomodoro_complete " #[fg=$color_green] "
+    #tmux set -g @pomodoro_ask_break " #[fg=$color_gray] break?"
 
     ###
     # OPTIONS
@@ -60,22 +62,22 @@ main() {
     tmux set -g status-position bottom
     tmux set -g status-left-length 100
     tmux set -g status-right-length 100
-    tmux set -g status-style "bg=$color_fg"
+    tmux set -g status-style "bg=$color_bg,fg=$color_fg"
 
-    tmux set -g pane-active-border fg=$color_gray
-    tmux set -g pane-border-style fg=$color_gray
+    tmux set -g pane-active-border fg="$color_green"
+    tmux set -g pane-border-style fg="$color_border"
 
-    tmux set -g message-style bg=$color_blue,fg=$color_bg
+    tmux set -g message-style "bg=$color_blue,fg=$color_bg"
     tmux setw -g window-status-separator "  "
-    tmux set-window-option -g mode-style bg=$color_purple,fg=$color_bg
+    tmux set-window-option -g mode-style bg="$color_magenta,fg=$color_bg"
 
     ###
     # FORMAT
 
-    tmux set -g status-left "#{tmux_mode_indicator}#[bg=$color_selection] #S #[default]#[fg=$color_gray]#{pomodoro_status} "
+    tmux set -g status-left "#{tmux_mode_indicator}#[bg=$color_bg] #S #[default]#[fg=$color_gray]#{pomodoro_status} "
     tmux set -g status-right "#[fg=$color_gray]#{ram_percentage} | #{cpu_percentage} | %a %d-%b %Y %H:%M %Z"
     tmux setw -g window-status-format "#[fg=$color_gray]#I: #[noitalics]#W"
-    tmux setw -g window-status-current-format "#[fg=$color_purple]#I: #[fg=$color_buffer,noitalics,bold]#W"
+    tmux setw -g window-status-current-format "#[fg=$color_magenta]#I: #[fg=$color_buffer,noitalics,bold]#W"
 }
 
 main
